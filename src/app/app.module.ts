79b0858151b83877d383a6649007e219f0a1f710
sarea <sareakk@gmail.com>
2017-10-27 10:26:46 +0200

    init dev inv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+// Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

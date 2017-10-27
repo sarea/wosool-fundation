e4a46de058472e0781aceb2ac02cb36c8d776770
sarea <sareakk@gmail.com>
2017-10-28 00:53:31 +0200

    nav bar functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,52 @@
 import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 // Material
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatSidenavModule,
+} from '@angular/material';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    PartnersComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    RouterModule.forRoot([
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'partners', component: PartnersComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: '**', redirectTo: 'home', pathMatch: 'full'}
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
